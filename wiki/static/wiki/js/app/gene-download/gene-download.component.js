463d3cfaf58584bfb3c56163ced7e02ea4e1ab5b
Tim Putman <putmantime@gmail.com>
2017-02-09 16:18:48 -0800

    adjustments to annotations accordion

--- wiki/static/wiki/js/app/gene-download/gene-download.component.js
+++ wiki/static/wiki/js/app/gene-download/gene-download.component.js
@@ -1,30 +1,41 @@
 angular
     .module('geneDownload')
     .component('geneDownload', {
-        templateUrl: '/static/wiki/js/angular_templates/gene-download.html',
+        template: '<a target="_blank" href="{{$ctrl.genesUrl()}}"><div class="btn btn-default">Get Genes</div></a>',
 
         bindings: {
             taxid: '<'
         },
-        controller: function (allOrgGenes) {
+        controller: function () {
 
             var ctrl = this;
 
+
             ctrl.$onChanges = function (changesObj) {
-
-
                 if (changesObj.taxid) {
-                    ctrl.exportJson = function () {
-                        allOrgGenes.getAllOrgGenes(ctrl.taxid).then(function (data) {
-                            var preData=  data.data.results.bindings;
-                            var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(preData));
-                            var dlAnchorElem = document.getElementById('downloadAnchorElem');
-                            dlAnchorElem.setAttribute("href", dataStr);
-                            dlAnchorElem.setAttribute("download", ctrl.taxid + "genes.json");
-                        });
-                    }
-                }
-            };
 
+                    ctrl.genesUrl = function () {
+                        return "https://query.wikidata.org/#SELECT ?refSeqChromosome " +
+                            "?gene ?genStart ?genEnd ?strand ?geneLabel ?entrez ?locusTag ?protein " +
+                            "?proteinLabel ?uniprot ?refseqProt" +
+                            " WHERE{ ?taxon wdt:P685 '" + ctrl.taxid + "'. " +
+                            "?gene wdt:P703 ?taxon; " +
+                            "wdt:P279 wd:Q7187; " +
+                            "wdt:P644 ?genStart; " +
+                            "wdt:P645 ?genEnd; " +
+                            "wdt:P2548 ?strand; " +
+                            "wdt:P2393 ?locusTag; " +
+                            "wdt:P351 ?entrez; " +
+                            "wdt:P688 ?protein. " +
+                            "?protein wdt:P352 ?uniprot; " +
+                            "wdt:P637 ?refseqProt." +
+                            "OPTIONAL{ ?gene p:P644 ?chr. ?chr pq:P2249 ?refSeqChromosome.} " +
+                            "SERVICE wikibase:label { bd:serviceParam wikibase:language 'en' . } }";
+                    }
+
+                }
+
+            };
         }
+
     });
