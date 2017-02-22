28c54f15b0952d4fd06d3f1e96675533108473db
Tim Putman <putmantime@gmail.com>
2017-02-22 09:41:49 -0800

    org tree on landing page

--- wiki/static/wiki/js/app/organism-tree/organism-tree.component.js
+++ wiki/static/wiki/js/app/organism-tree/organism-tree.component.js
@@ -14,49 +14,53 @@ angular
 
                     // For each orgchart box, provide the name, manager, and tooltip to show.
                     data.addRows([
-                        [{v: 'Chlamydia', f: '<div style="color:#385d94; font-style:italic">Chlamydia</div>'},
+                        [{v: 'Chlamydia', f: '<div class="btn btn-default treeNode nohover" >Chlamydia</div>'},
                             ''],
                         [{
                             v: 'Chlamydia trachomatis',
-                            f: '<div style="color:#385d94; font-style:italic">Chlamydia trachomatis</div>'
+                            f: '<div class="btn btn-default treeNode nohover" >Chlamydia trachomatis</div>'
                         },
                             'Chlamydia'],
                         [{
                             v: 'Chlamydia muridarum',
-                            f: '<div style="color:#385d94; font-style:italic">Chlamydia muridarum</div>'
+                            f: '<div class="btn btn-default treeNode nohover" >Chlamydia muridarum</div>'
                         },
                             'Chlamydia'],
                         [{
                             v: 'Chlamydia pneumoniae',
-                            f: '<div style="color:#385d94; font-style:italic">Chlamydia pneumoniae</div>'
+                            f: '<div class="btn btn-default treeNode nohover" >Chlamydia pneumoniae</div>'
                         },
                             'Chlamydia'],
                         [{
                             v: 'Chlamydia pneumoniae CWL029',
-                            f: '<a href="/organism/115713/" style="color:#385d94; font-style:italic">Chlamydia pneumoniae CWL029</div>'
+                            f: '<a class="btn btn-default treeNode" href="/organism/115713/" >Chlamydia pneumoniae CWL029</div>'
                         },
                             'Chlamydia pneumoniae'],
                         [{
                             v: 'Chlamydia muridarum Nigg',
-                            f: '<a href="/organism/243161/"  style="color:#385d94; font-style:italic">Chlamydia muridarum Nigg</div>'
+                            f: '<a class="btn btn-default treeNode" href="/organism/243161/" >Chlamydia muridarum Nigg</div>'
                         },
                             'Chlamydia muridarum'],
                         [{
                             v: 'Chlamydia trachomatis 434/BU',
-                            f: '<a href="/organism/471472/" style="color:#385d94; font-style:italic">Chlamydia trachomatis 434/BU</a>'
+                            f: '<a  class="btn btn-default treeNode" href="/organism/471472/" >Chlamydia trachomatis 434/BU</a>'
                         },
                             'Chlamydia trachomatis'],
                         [{
                             v: 'Chlamydia trachomatis D/UW-3/CX',
-                            f: '<a href="/organism/272561/" style="color:#385d94; font-style:italic">Chlamydia trachomatis D/UW-3/CX</>'
+                            f: '<a  class="btn btn-default treeNode" href="/organism/272561/" >Chlamydia trachomatis D/UW-3/CX</>'
                         },
                             'Chlamydia trachomatis']
 
 
                     ]);
-
+                    var options = {
+                        nodeClass: 'none',
+                        allowHtml: true
+                    };
                     // Create the chart.
                     var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
+                    console.log(chart);
                     // Draw the chart, setting the allowHtml option to true for the tooltips.
                     chart.draw(data, {allowHtml: true});
                 }
