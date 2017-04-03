3b1918ec278614c199550d21746851f6043dfc48
Tim Putman <putmantime@gmail.com>
2017-04-03 09:03:27 -0700

    switched to  from parsing URL with

--- wiki/static/wiki/js/app/browser-page/browser-page.component.js
+++ wiki/static/wiki/js/app/browser-page/browser-page.component.js
@@ -1,14 +1,15 @@
 angular
     .module('browserPage')
     .component('browserPage', {
-        controller: function ($filter, $location, allChlamOrgs, allOrgGenes) {
+        controller: function ($filter, $location, $routeParams, allChlamOrgs, allOrgGenes) {
             //Browser page Component.  Directed here to paginated list of genes when organism is selected from landing page,
             //or when browser is pointed to URL with /organism/<valid-taxid>
             //The component parses the URL to determine the current organism taxid and uses that to look up all genes and load
             //them into pagination, as well as launches JBrowse
             var ctrl = this;
             ctrl.$onInit = function () {
-                ctrl.currentTaxid = $location.path().split("/")[2];
+                ctrl.currentTaxid = $routeParams.taxid;
+                console.log(ctrl.currentTaxid);
                 allChlamOrgs.getAllOrgs(function (data) {
                     ctrl.orgList = data;
                     ctrl.currentOrg = $filter('getJsonItemOrg')('taxid', ctrl.currentTaxid, ctrl.orgList);
@@ -20,8 +21,6 @@ angular
                 allOrgGenes.getAllOrgGenes(ctrl.currentTaxid)
                     .then(function (data) {
                         var dataResults = data.data.results.bindings;
-                        console.log(dataResults[0]);
-                        console.log('hello');
                         ctrl.currentAllGenes = $filter('orderObjectBy')(dataResults, 'genStart');
                         ctrl.initialGene = ctrl.currentAllGenes[0];
 
