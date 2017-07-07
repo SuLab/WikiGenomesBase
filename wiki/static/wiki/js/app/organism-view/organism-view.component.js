4f6fcf4f0e417d2612ec62fb104cdfd266896b9e
Tim Putman <putmantime@gmail.com>
2017-07-07 11:20:07 -0700

    genome level link on main page

--- wiki/static/wiki/js/app/organism-view/organism-view.component.js
+++ wiki/static/wiki/js/app/organism-view/organism-view.component.js
@@ -2,9 +2,9 @@ angular
     .module('organismView')
     .component('organismView', {
         templateUrl: '/static/wiki/js/angular_templates/organism-view.html',
-        controller: function () {
+        controller: function ($routeParams) {
             var ctrl = this;
-
+            ctrl.currentTaxid = $routeParams.taxid;
         },
         bindings: {
             org: '<'
