34d6acb521f252b1db5fd5c1b45644b60bae6828
djow2019 <derek.jow@gmail.com>
2017-09-13 12:46:41 -0700

    Added Checkbox column for orthologs; Added toggleable div that displays a temporary MSA Viewer

--- wiki/static/wiki/js/app/annotations-view/ortholog-view/ortholog-view.component.js
+++ wiki/static/wiki/js/app/annotations-view/ortholog-view/ortholog-view.component.js
@@ -4,10 +4,54 @@ angular
         bindings: {
             data: '<'
         },
-        controller: function ($filter) {
-            var ctrl = this;
-            ctrl.$onInit = function () {
-            };
+        controller: function ($scope) {
+
+	    // for selecting from the check list
+	    $scope.select = function(item) {
+		item.selected = !item.selected;
+	    };
+
+	    // creates a list of the selected orthologs
+	    $scope.updateSelected = function(data) {
+		$scope.selected = [];
+		angular.forEach(data, function(value, key) {
+			if (value.selected) {
+				$scope.selected.push(key);
+			}
+		});
+		console.log($scope.selected);
+	    };
+	
+            // the bootstrap menu at the top of the widget'
+	    $scope.menu = {
+		    el: document.getElementById("msaDiv"),
+		    vis: {
+		      conserv: false,
+		      overviewbox: false
+		    },
+		    // smaller menu for JSBin
+		    menu: "small",
+		    bootstrapMenu: true
+	    };
+	    
+	    // the viewing panel
+	    $scope.msa = new msa.msa($scope.menu);
+
+	    // the function to render the viewing panel
+	    $scope.align = function() {
+		  $scope.msa.render();
+	    };
+
+	    // function to update the selected list and align after
+	    $scope.updatePanel = function(data) {
+		$scope.updateSelected(data);
+		$scope.align();
+	    };
+
+	    // set up the data
+	    $scope.msa.u.file.
+		importURL("http://rostlab.org/~goldberg/jalv_example.clustal", 	    			$scope.align);
+	    
         },
         templateUrl: '/static/wiki/js/angular_templates/ortholog-view.html'
     });
