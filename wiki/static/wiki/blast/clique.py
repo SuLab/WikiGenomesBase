c8c411fd789dc06e5b7957f1fb7601b6a30b7ab1
djow2019 <derek.jow@gmail.com>
2017-12-14 19:23:45 -0800

    Added Single Detail File

--- wiki/static/wiki/blast/clique.py
+++ wiki/static/wiki/blast/clique.py
@@ -16,8 +16,8 @@ clique4 = pd.DataFrame(columns=strains)
 clique3 = pd.DataFrame(columns=strains)
 clique2 = pd.DataFrame(columns=strains)
 
-# extra details about strains with multiple hits for 3 clique
-outfile = open("results/cliques_3_details.txt", "w")
+# extra details about strains with multiple or missing hits
+outfile = open("results/cliques_details.txt", "w")
 cliqDetails = pd.DataFrame(columns=["Query", "Subject", "% Similarity", "E Value", "Length"])
 
 # to determine the strain from the locus tag
@@ -34,14 +34,14 @@ def getStrain(tag):
     return "Unknown"
 
 # find the missing strain in a given dataframe row
-def getMissingStrain(row):
+def getMissingStrains(row):
+  missing = []
   for i, j in zip(row, strains):
     if i == "[]":
-      return j
-  return "Unknown"
+      missing.append(j)
+  return missing
 
-line = 1
-hist = []
+#hist = []
 # now iterate through each row
 for index, row in data.iterrows():
   size = 0
@@ -53,13 +53,63 @@ for index, row in data.iterrows():
     if len(l) > 1:
       multiple = True
 
+  # generate the details file
+  outfile.write("Line " + str(index + 1) + ": " + str(row.values) + "\n\n")
+
+  # get list of closest hits to missing  terms
+  if size != 4:
+    missing = getMissingStrains(row.values)
+    outfile.write("\tMissing strain(s): %s\n" % str(missing))
+    outfile.write("\tQuery Subject %_Similarity E_Value Length\n")
+
+    # list the best hits for the missing strain v all others
+    terms = []
+    for i in row.values:
+      l = ast.literal_eval(i)
+      for item in l:
+        terms.append(item)
+
+    for m in missing:
+      for query in terms:
+        temp = pd.read_table("parsed_%s_v_%s.tab" % (m, getStrain(query)), header=0)
+        rows = temp.loc[(temp["Query"] == query)]
+        for index, target in rows.iterrows():
+          outfile.write("\t%s\n" % str(target.values))
+            
+          # now do the reverse search
+          temp = pd.read_table("parsed_%s_v_%s.tab" % (getStrain(target["Query"]), getStrain(target["Subject"])), header=0)
+          other = temp.loc[(temp["Query"] == target["Subject"])]
+          for i, t in other.iterrows():
+            outfile.write("\t%s\n" % str(t.values))
+    outfile.write("\n")
+
+  # get a list of all terms to search for multiple hits
+  terms = []
+  if multiple:
+    outfile.write("\tRepeated Hits\n")
+    outfile.write("\tQuery Subject %_Similarity E_Value Length\n")
+    for i in row.values:
+      l = ast.literal_eval(i)
+      for item in l:
+        terms.append(item)
+
+    # now get all the data for the individual comparisons in the repeated strains
+    for query in terms:
+      for subject in terms:
+        if getStrain(query) != getStrain(subject):
+          temp = pd.read_table("parsed_%s_v_%s.tab" % (getStrain(subject), getStrain(query)), header=0)
+          rows = temp.loc[(temp["Query"] == query) & (temp["Subject"] == subject)]
+          for index, target in rows.iterrows():
+            outfile.write("\t%s\n" % str(target.values))
+    outfile.write("\n")
+
   if size == 4 and not multiple:
     clique4 = clique4.append(pd.DataFrame([row.values], columns=strains))
   elif size >= 3:
     clique3 = clique3.append(pd.DataFrame([row.values], columns=strains))
     
     # add e value to histogram
-    terms = []
+    """terms = []
     for i in row.values:
       l = ast.literal_eval(i)
       for item in l:
@@ -74,57 +124,7 @@ for index, row in data.iterrows():
             if val > 0:
               hist.append(log10(val))
             else:
-              hist.append(-200)
-
-    line = line + 1
-
-    outfile.write("Line " + str(line) + ": " + str(row.values) + "\n\n")
-
-    # get list of closest hits to missing  term
-    if size == 3:
-      missing = getMissingStrain(row.values)
-      outfile.write("\tMissing strain: %s\n" % missing)
-      outfile.write("\tQuery Subject %_Similarity E_Value Length\n")
-
-      # list the best hits for the missing strain v all others
-      terms = []
-      for i in row.values:
-        l = ast.literal_eval(i)
-        for item in l:
-          terms.append(item)
-      for query in terms:
-        if getStrain(query) != missing:
-          temp = pd.read_table("parsed_%s_v_%s.tab" % (missing, getStrain(query)), header=0)
-          rows = temp.loc[(temp["Query"] == query)]
-          for index, target in rows.iterrows():
-            outfile.write("\t%s\n" % str(target.values))
-            
-	    # now do the reverse search
-            temp = pd.read_table("parsed_%s_v_%s.tab" % (getStrain(target["Query"]), getStrain(target["Subject"])), header=0)
-            other = temp.loc[(temp["Query"] == target["Subject"])]
-            for i, t in other.iterrows():
-              outfile.write("\t%s\n" % str(t.values))
-      outfile.write("\n")
-
-    # get a list of all terms to search for
-    terms = []
-    if multiple:
-      outfile.write("\tRepeated Hits\n")
-      outfile.write("\tQuery Subject %_Similarity E_Value Length\n")
-      for i in row.values:
-        l = ast.literal_eval(i)
-        for item in l:
-          terms.append(item)
-
-      # now get all the data for the individual comparisons in the repeated strains
-      for query in terms:
-        for subject in terms:
-          if getStrain(query) != getStrain(subject):
-            temp = pd.read_table("parsed_%s_v_%s.tab" % (getStrain(subject), getStrain(query)), header=0)
-            rows = temp.loc[(temp["Query"] == query) & (temp["Subject"] == subject)]
-            for index, target in rows.iterrows():
-              outfile.write("\t%s\n" % str(target.values))
-      outfile.write("\n")
+              hist.append(-200)"""
 
   elif size == 2:
     clique2 = clique2.append(pd.DataFrame([row.values], columns=strains))
@@ -132,10 +132,10 @@ for index, row in data.iterrows():
 outfile.close()
 
 # draw the histogram
-print ("Displaying Histogram")
-plt.hist(hist)
+#print ("Displaying Histogram")
+#plt.hist(hist)
 #, bins=np.arange(min(hist), max(hist) + 10, 10))
-plt.show()
+#plt.show()
 
 # now save the cliques to file
 clique4.to_csv("results/cliques_4_rbm.tab", index=False, header=True, sep="\t")
