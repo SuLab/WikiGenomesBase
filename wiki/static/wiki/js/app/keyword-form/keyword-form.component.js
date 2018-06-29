ed04194c29a6217c30013dd12d94535ce8f07a37
djow2019 <derek.jow@gmail.com>
2018-06-29 16:51:29 -0700

    Improved functionality

--- wiki/static/wiki/js/app/keyword-form/keyword-form.component.js
+++ wiki/static/wiki/js/app/keyword-form/keyword-form.component.js
@@ -1,14 +1,17 @@
 angular
     .module('keywordForm')
     .component('keywordForm', {
-        bindings: {
-            taxid: '<',
-            genes: '<'
-        },
-        controller: function ($filter, $location) {
+        controller: function ($filter, $location, allChlamydiaGenes) {
             'use strict';
             var ctrl = this;
             ctrl.$onInit = function(){
+            	
+            	ctrl.selected = null;
+            	
+            	allChlamydiaGenes.getAllChlamGeneLabels().then(function(data) {
+            		ctrl.genes = data.data.results.bindings;
+            	});
+            	
                 ctrl.submitKeyword = function ($item) {
                     if ($item == undefined){
                         alert("Please enter a keyword or ID");
@@ -18,9 +21,37 @@ angular
                 };
                 
                 ctrl.onSelect = function ($item) {
-                    $location.path('/organism/' + ctrl.taxid + "/gene/" + $item.locusTag.value);
+                    $location.path('/organism/' + $item.taxid.value + "/gene/" + $item.locusTag.value);
                 };
             };
+            
+            // whether or not user has scrolled through dropdown
+            ctrl.scrolled = false;
+            
+            ctrl.keyPressed = function($event) {
+            	
+            	// remove / from keyword
+            	ctrl.keyword = ctrl.keyword.replace("/", " ");
+            	
+            	// ENTER is pressed
+            	if ($event.keyCode == 13){ 
+            		
+            		// if user has scrolled, select that item
+            		if (ctrl.scrolled) {
+            			ctrl.onSelect(ctrl.selected);
+            		
+            		// else go to advanced search
+            		} else {
+            			ctrl.submitKeyword(ctrl.keyword);
+            		}
+            		
+            		// any of the arrowkeys pressed
+            	} else if ($event.keyCode >=37 && $event.keyCode <= 40) {
+            		ctrl.scrolled = true;
+            	}
+            	
+            };
+
         },
         templateUrl: '/static/wiki/js/angular_templates/keyword-form.html'
     });
