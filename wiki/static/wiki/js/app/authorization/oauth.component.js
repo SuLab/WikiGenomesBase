7602ce6753a204029f167d8085ba003c34cd42dc
djow2019 <derek.jow@gmail.com>
2018-07-03 14:33:14 -0700

    Added authorize form

--- wiki/static/wiki/js/app/authorization/oauth.component.js
+++ wiki/static/wiki/js/app/authorization/oauth.component.js
@@ -18,39 +18,8 @@ angular
 
                 $location.url($location.path());
             }
-            if ($location.path().includes('authorized')) {
-                ctrl.authorization = true;
-            } else {
-                ctrl.authorization = false;
-            }
-
-            ctrl.oauthAuthorization = function () {
-                sendToView.sendToView(
-                    '/wd_oauth',
-                    {
-                        'authorization': 'sending',
-                        'current_path': $location.path(),
-                        'initiate': true
-                    })
-                    .then(function (data) {
-                        $window.location.href = data.data.wikimediaURL;
-                    });
-            };
-
-            ctrl.revokeAuthorization = function () {
-                sendToView.sendToView(
-                    '/wd_oauth',
-                    {
-                        'deauthenticate': true
-                    })
-                    .then(function (data) {
-                        var org = '/organism/{taxid}'.replace('{taxid}', $routeParams.taxid);
-                        var locusTag = '/gene/{locusTag}/'.replace('{locusTag}', $routeParams.locusTag);
-                        $location.url(org + locusTag);
-                    });
-
-            };
-
+            
+            ctrl.authorization = $location.path().includes('authorized');
 
         },
         templateUrl: '/static/wiki/js/angular_templates/authorization-view.html'
