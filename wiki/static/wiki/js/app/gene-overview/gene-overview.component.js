319f85106e7d90bea6473c86d363ada5a0e08bef
djow2019 <derek.jow@gmail.com>
2018-07-16 16:16:44 -0700

    Fixed mutant counts

--- wiki/static/wiki/js/app/gene-overview/gene-overview.component.js
+++ wiki/static/wiki/js/app/gene-overview/gene-overview.component.js
@@ -6,16 +6,27 @@ angular
             data: '<',
             annotations: '<'
         },
-        controller: function ($routeParams) {
+        controller: function ($routeParams, sendToView, $location) {
             'use strict';
             var ctrl = this;
             ctrl.$onInit = function () {
             	ctrl.taxid = $routeParams.taxid;
-            	
+            	ctrl.cm = ctrl.tm = ctrl.rm = ctrl.im = 0;
             };
             
-            ctrl.$onChanges = function() {
-            	console.log(ctrl.annotations);
-            };
+            var url_surf = $location.path() + '/mg_mutant_view';
+            
+		   	sendToView.sendToView(url_surf, {"action" : "chemical"}).then(function(data) {
+		        ctrl.cm = data.data.mutants.length;
+		    });
+		   	sendToView.sendToView(url_surf, {"action" : "transposition"}).then(function(data) {
+	        	ctrl.tm = data.data.mutants.length;
+		   	});
+		   	sendToView.sendToView(url_surf, {"action" : "recombination"}).then(function(data) {
+	        	ctrl.rm = data.data.mutants.length;
+		   	});
+		   	sendToView.sendToView(url_surf, {"action" : "insertion"}).then(function(data) {
+	        	ctrl.im = data.data.mutants.length;
+		   	});
         }
     });
