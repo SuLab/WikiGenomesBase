ff4f1a580c9da06839cf357f95e3666a10b08833
djow2019 <derek.jow@gmail.com>
2018-07-17 09:52:49 -0700

    Testing mutants

--- wiki/static/wiki/js/app/gene-overview/gene-overview.component.js
+++ wiki/static/wiki/js/app/gene-overview/gene-overview.component.js
@@ -6,16 +6,20 @@ angular
             data: '<',
             annotations: '<'
         },
-        controller: function ($routeParams, sendToView) {
+        controller: function ($routeParams, sendToView, $location) {
             'use strict';
             var ctrl = this;
             ctrl.$onInit = function () {
             	ctrl.taxid = $routeParams.taxid;
+            	ctrl.locusTag = $routeParams.locusTag;
             	ctrl.cm = ctrl.tm = ctrl.rm = ctrl.im = 0;
             };
             
             ctrl.$onChanges = function() {
+            	console.log("FROM ON CHANGES");
+            	console.log(ctrl.annotations);
             	angular.forEach(ctrl.annotations.mutants, function(x) {
+            		console.log(x);
             		if (x.mutation_id=='EFO_0000370') {
             			ctrl.cm++;
             		} else if (x.mutation_id=='EFO_0004021') {
@@ -27,6 +31,16 @@ angular
             		}
             	});
             };
+            
+            var url_suf = $location.path() + '/mg_mutant_view';
+
+            sendToView.sendToView(url_suf, {
+                locusTag : ctrl.locusTag,
+                taxid : ctrl.taxid
+            }).then(function(data) {
+            	console.log("HERE");
+                console.log(data.data.mutants);
+            });
             
         }
     });
