c840d1daa9b96825ab898226f61ad59f33122d8d
djow2019 <derek.jow@gmail.com>
2018-07-17 10:15:52 -0700

    mutants

--- wiki/static/wiki/js/app/gene-overview/gene-overview.component.js
+++ wiki/static/wiki/js/app/gene-overview/gene-overview.component.js
@@ -6,41 +6,32 @@ angular
             data: '<',
             annotations: '<'
         },
-        controller: function ($routeParams, sendToView, $location) {
+        controller: function ($routeParams, sendToView, $location, $scope) {
             'use strict';
             var ctrl = this;
             ctrl.$onInit = function () {
             	ctrl.taxid = $routeParams.taxid;
             	ctrl.locusTag = $routeParams.locusTag;
             	ctrl.cm = ctrl.tm = ctrl.rm = ctrl.im = 0;
+            	
+                var url_suf = $location.path() + '/mg_mutant_view';
+                sendToView.sendToView(url_suf, {
+                    locusTag : ctrl.locusTag,
+                    taxid : ctrl.taxid
+                }).then(function(data) {
+                	angular.forEach(data.data.mutants, function(x) {
+                		if (x.mutation_id=='EFO_0000370') {
+                			ctrl.cm++;
+                		} else if (x.mutation_id=='EFO_0004021') {
+                			ctrl.tm++;
+                		} else if (x.mutation_id=='EFO_0004016') {
+                			ctrl.im++;
+                		} else if (x.mutation_id=='EFO_0004293'){
+                			ctrl.rm++;
+                		}
+                	});
+                });
+                
             };
-            
-            ctrl.$onChanges = function() {
-            	console.log("FROM ON CHANGES");
-            	console.log(ctrl.annotations);
-            	angular.forEach(ctrl.annotations.mutants, function(x) {
-            		console.log(x);
-            		if (x.mutation_id=='EFO_0000370') {
-            			ctrl.cm++;
-            		} else if (x.mutation_id=='EFO_0004021') {
-            			ctrl.tm++;
-            		} else if (x.mutation_id=='EFO_0004016') {
-            			ctrl.im++;
-            		} else if (x.mutation_id=='EFO_0004293'){
-            			ctrl.rm++;
-            		}
-            	});
-            };
-            
-            var url_suf = $location.path() + '/mg_mutant_view';
-
-            sendToView.sendToView(url_suf, {
-                locusTag : ctrl.locusTag,
-                taxid : ctrl.taxid
-            }).then(function(data) {
-            	console.log("HERE");
-                console.log(data.data.mutants);
-            });
-            
         }
     });
