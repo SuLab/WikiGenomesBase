3df2cef9cca5139563336c0233f844bb87a9cc30
djow2019 <derek.jow@gmail.com>
2018-07-26 17:05:49 -0700

    file cleanup

--- application_settings.py
+++ application_settings.py
@@ -1,12 +1,27 @@
-# Application Specific Information
+# Application Name
 title = 'ChlamBase'
+
+# enter unique db name for your database
 mongo_database = "chlamdb"
 
-# Configurations for django settings.py
-allowed_hosts = ['127.0.0.1', 'localhost']
-
-# TIME_ZONE
+# used for celery beat scheduler
 wg_timezone = 'America/Los_Angeles'
 
-# Taxids of the organisms in the database (used for setup script)
+# Taxids of the organisms in the database (used for setup script, routing, jbrowse)
 taxids = ['471472', '272561', '243161', '115713']
+
+# which annotations to allow on the gene page
+modules = {
+    "protein-view": True,
+    "ortholog-view": True,
+    "alignment-view": True,
+    "expression-view": True,
+    "function-view": True,
+    "localization-view": True,
+    "operon-view": True,
+    "interpro-view": True,
+    "enzyme-view": True,
+    "mutant-view": True,
+    "protein-interaction-view": True,
+    "related-publication-view": True
+}
