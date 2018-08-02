00e192629881efdb00239cc791f650dcd4e9004d
djow2019 <derek.jow@gmail.com>
2018-08-01 18:20:19 -0700

    Implemented ng table for operon view

--- wiki/static/wiki/js/app/annotations-view/operon-view/operon-view.component.js
+++ wiki/static/wiki/js/app/annotations-view/operon-view/operon-view.component.js
@@ -7,9 +7,30 @@ angular
             allorggenes: '<',
             gene: '<'
         },
-        controller: function () {
+        controller: function (NgTableParams) {
             var ctrl = this;
-            ctrl.$onInit = function () {
+            
+            ctrl.$onChanges = function() {
+                if (ctrl.operon) {
+                	var parsed = [];
+                	angular.forEach(ctrl.operon, function(obj) {
+                		var next = {};
+                		angular.forEach(obj, function(value, key) {
+                			next[key] = value.value;
+                		});
+                		parsed.push(next);
+                	});
+                	
+	                ctrl.tableParams = new NgTableParams(
+	                	{
+	                		page:1,
+	                		count: 10
+	                	},
+	                	{
+	                		dataset: parsed
+	                	}
+	                );
+                }
             };
         }
     });
