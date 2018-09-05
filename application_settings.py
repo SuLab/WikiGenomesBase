40a6f807c4f5cc2222ddd852fafcd73d0f5a1fc4
djow2019 <derek.jow@gmail.com>
2018-09-04 17:16:47 -0700

    fish base settings

--- application_settings.py
+++ application_settings.py
@@ -1,11 +1,11 @@
 # enter unique db name for your database
-mongo_database = "chlamdb"
+mongo_database = "fishdb"
 
 # used for celery beat scheduler
 wg_timezone = 'America/Los_Angeles'
 
 # Taxids of the organisms in the database (used for setup script, routing, jbrowse)
-taxids = ['471472', '272561', '243161', '115713']
+taxids = ['105023']
 
 # which annotations to allow on the gene page
 modules = {
@@ -24,65 +24,32 @@ modules = {
 }
 
 application = {
-    "title": "ChlamBase",
-    "noun": "Chlamydia",
-    "adjective": "Chlamydial",
-    "parent_taxid": 810,
-    "newsfeed_search_term": "chlamydia trachomatis",
+    "title": "FishBase",
+    "noun": "Turqoise Killifish",
+    "adjective": "Killifish",
+    "parent_taxid": 28779,
+    "newsfeed_search_term": "turqoise killifish",
     "newsfeed_recent_days": 10,
     "newsfeed_max_articles": 20,
-    "example_locus_tag": "CTL0260",
+    "example_locus_tag": "LOC107381016 ",
 
     # Either locus_tag or entrez
-    "primary_identifier": "locus_tag"
+    "primary_identifier": "entrez"
 }
 
 tax2NameMap = {
-    "471472": "Chlamydia trachomatis 434/BU",
-    "272561": "Chlamydia trachomatis D/UW-3/CX",
-    "243161": "Chlamydia muridarum Str. Nigg",
-    "115713": "Chlamydia pneumoniae CWL209"
+    "105023": "Nothobranchius furzeri"
 }
 
 # google chart organism tree
 orgTree = {
     'data': [
-                [{'v': 'Chlamydia', 'f': '<div class="btn btn-default treeNode nohover" ><i>Chlamydia</i></div>'},
+                [{'v': 'Nothobranchius', 'f': '<div class="btn btn-default treeNode nohover" ><i>Nothobranchius</i></div>'},
                     ''],
                 [{
-                    'v': 'Chlamydia trachomatis',
-                    'f': '<div class="btn btn-default treeNode nohover" ><i>Chlamydia trachomatis</i></div>'
+                    'v': 'Nothobranchius furzeri',
+                    'f': '<div class="btn btn-default treeNode link"><a href="/organism/105023/" ><i>Nothobranchius furzeri</i></div>'
                 },
-                    'Chlamydia'],
-                [{
-                    'v': 'Chlamydia muridarum',
-                    'f': '<div class="btn btn-default treeNode nohover" ><i>Chlamydia muridarum</i></div>'
-                },
-                    'Chlamydia'],
-                [{
-                    'v': 'Chlamydia pneumoniae',
-                    'f': '<div class="btn btn-default treeNode nohover" ><i>Chlamydia pneumoniae</i></div>'
-                },
-                    'Chlamydia'],
-                [{
-                    'v': 'Chlamydia pneumoniae CWL029',
-                    'f': '<div class="btn btn-default treeNode link"><a href="/organism/115713/" ><i>Chlamydia pneumoniae CWL029</i></div>'
-                },
-                    'Chlamydia pneumoniae'],
-                [{
-                    'v': 'Chlamydia muridarum Nigg',
-                    'f': '<div class="btn btn-default treeNode link"><a href="/organism/243161/" ><i>Chlamydia muridarum Nigg</i></div>'
-                },
-                    'Chlamydia muridarum'],
-                [{
-                    'v': 'Chlamydia trachomatis 434/BU',
-                    'f': '<div class="btn btn-default treeNode link"><a href="/organism/471472/" ><i>Chlamydia trachomatis 434/BU</i></a></div>'
-                },
-                    'Chlamydia trachomatis'],
-                [{
-                    'v': 'Chlamydia trachomatis D/UW-3/CX',
-                    'f': '<div class="btn btn-default treeNode link"><a href="/organism/272561/" ><i>Chlamydia trachomatis D/UW-3/CX</i></a></div>'
-                },
-                    'Chlamydia trachomatis']
+                    'Nothobranchius'],
             ]
 }
