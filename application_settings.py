dee1b9d430208df0deaa5fc54d189844c9946699
djow2019 <derek.jow@gmail.com>
2018-09-25 16:28:14 -0700

    Updated strain descriptions on landing page

--- application_settings.py
+++ application_settings.py
@@ -51,24 +51,24 @@ strains = [
     [
         {
             "title": "Chlamydia trachomatis 434/BU",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "description": "The LGV strains of CT are associated with aggressive genital tract disease that can disseminate to lymph nodes. The epidemiologic and pathologic differences between these strains and the classical urogenital strains are significant, but the genomic distinctions responsible for these differences are not clear.  This strain is a common model organism used by many to study chlamydial infection in vitro and in vivo.",
             "taxid": "471472"
         },
         {
             "title": "Chlamydia trachomatis D/UW-3CX",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "description": "The strain represents the classical urogenital strains that are the most common reportable infection in the United States. Its genome is very similar to the LGV strains and to a lesser extent, cm, but there remains a poor level of understanding of the differences in pathogenesis by these organisms.",
             "taxid": "272561"
         }
     ],
     [
         {
             "title": "Chlamydia muridarum Nigg",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "description": "CM is a murine pathogen that can infect most mucosal surfaces in the mouse. Because it is genetically quite similar to CT, it is often used as a model of human CT infection. There is only a minimal understanding of the molecular basis of tropism differences in these otherwise highly related species. The genomes of these chlamydiae are highly syntenous except for a large region termed the plasticity zone.",
             "taxid": "243161"
         },
         {
             "title": "Chlamydia pneumoniae CWL029",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "description": "Cpn is a pathogen that infects humans and a variety of other animal species. In humans, this organism causes a walking pneumonia, and has been linked to chronic diseases such as hardening of the arteries and neurologic problems. Some of these connections remain controversial. This pathogen represents a taxonomic lineage within the chlamydia that is different than c. trachomatis and C. Mur. Many of the fundamental properties of chlamydia are shared among these pathogens, but the genetic distinctions associated with the different diseases remain to be worked out.",
             "taxid": "115713"
         }
     ],
